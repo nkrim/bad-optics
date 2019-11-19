a119a3c2a3897d0893d874dcae32274cb81c88bb
nkrim62 <nkrim62@gmail.com>
2019-11-19 14:16:17 -0800

    full gulp integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 // Includes
-const { src, dest, parallel } = require('gulp');
+const { src, dest, series, parallel } = require('gulp');
+const del = require('del');
+// html
 const fs = require('fs');
 const pug = require('gulp-pug');
-// const less = require('gulp-less');
-// const minifyCSS = require('gulp-csso');
-// const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+// js
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+// css
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const minifyCSS = require('gulp-csso');
 
 // Globals
 const RELEASES_FILE = 'releases.json';
@@ -12,27 +21,44 @@ const RELEASES_FILE = 'releases.json';
 function html() {
 	let releases_config = JSON.parse(fs.readFileSync(RELEASES_FILE));
   	return src('index-template.pug')
+  		.pipe(rename('index.pug'))
     	.pipe(pug({
     		'pretty': true,
     		'locals': releases_config
     	}))
-    	.pipe(dest('.'))
+    	.pipe(dest('build'));
 }
 
-/*function css() {
-  return src('client/templates/*.less')
-    .pipe(less())
-    .pipe(minifyCSS())
-    .pipe(dest('build/css'))
+function css() {
+  	return src('src/css/*.css')
+		.pipe(sourcemaps.init())
+		.pipe(postcss([ autoprefixer() ]))
+		.pipe(minifyCSS())
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest('build/static/css'))
 }
 
 function js() {
-  return src('client/javascript/*.js', { sourcemaps: true })
-    .pipe(concat('app.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }))
+  	return src('src/js/*.js')
+  		.pipe(sourcemaps.init())
+	  	.pipe(babel())
+	    .pipe(concat('app.min.js'))
+	    .pipe(uglify())
+	    .pipe(sourcemaps.write("."))
+	    .pipe(dest('build/static/js'));
 }
 
+function resources() {
+	return src('resources/**/*').pipe(dest('build/'));
+}
+
+function clean() {
+	return del('build/*');
+}
+
+exports.clean = clean
+exports.resources = resources
 exports.js = js;
-exports.css = css;*/
+exports.css = css;
 exports.html = html;
-exports.default = html;//parallel(html, css, js);
+exports.default = series(clean, parallel(html, css, js, resources));
---
+++ src/js/resize.js
@@ -0,0 +1,26 @@
+// RESIZE SCRIPT
+// ==================================================
+const two_tile_width_max = 659;
+let prev_width = $(window).width();
+
+let resize_timeout = null;
+
+$(window).resize(function() {
+	// Check for width change, if no change then return
+	let width = $(window).width();
+	if(width === prev_width)
+		return;
+	prev_width = width;
+
+	// Quickly close all tiles
+	$('.tile-set').addClass('quick-close');
+	$('.tile').removeClass('show');
+	$('.tile-set').removeClass('quick-close');
+
+	// Set short timeout to ensure tile-content is closed before repositioning and resizing content
+	if(resize_timeout)
+		clearTimeout(resize_timeout);
+	resize_timeout = setTimeout(function() {
+		set_tile_content_container_positions();
+	}, 100);
+});
