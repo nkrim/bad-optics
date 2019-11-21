6f012231b95b823c00aa0b6a8880e10af6304af3
nkrim62 <nkrim62@gmail.com>
2019-11-21 02:52:12 -0800

    Semi-final working gulp workflow, up on site

--- gulpfile.js
+++ gulpfile.js
@@ -14,41 +14,48 @@ const uglify = require('gulp-uglify');
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const minifyCSS = require('gulp-csso');
+// upload
+const s3upload = require('gulp-s3-upload')({useIAM: true});
+
 
 // Paths
 const paths = {
-	'pug': {
-		'data': 'releases.json',
-		'index': 'index-template.pug',
-		'release': 'release-template.pug',
+	pug: {
+		data: 'releases.json',
+		index: 'index-template.pug',
+		release: 'release-template.pug',
 	},
-	'in': {
-		'css': 'src/css/*.css',
-		'js': ['src/js/*.js', '!src/js/index_exclude/*.js'],
-		'release_js': 
+	in: {
+		css: 'src/css/*.css',
+		js: ['src/js/*.js', '!src/js/index_exclude/*.js'],
+		release_js: 
 			[
 				'index_exclude/loaded_release.js',
 				'index_exclude/router_override.js',
 				'invert.js',
 				'scroll.js',
 			].map(p => 'src/js/'+p),
-		'resources': 'resources/**/*',
+		resources: 'resources/**/*',
 	},
-	'out': {
-		'base': 'build/',
-		'html': 'build/',
-		'css': 'build/static/css/',
-		'js': 'build/static/js/',
+	out: {
+		base: 'build/',
+		html: 'build/',
+		css: 'build/static/css/',
+		js: 'build/static/js/',
 	}
 }
 
 // Pug config
 const pug_config = {
-	'pretty': true,
+	// pretty: true,
 };
 
-// Pre-parse releases json
+// Pre-parse json
 const releases = JSON.parse(fs.readFileSync(paths.pug.data));
+
+// s3 Metadata
+const s3meta = {'uploaded-via': 'gulp-s3-upload'};
+const s3cache = 'max-age=315360000';
 
 // HTML - Pug
 function index() {
@@ -64,7 +71,7 @@ function release_pages() {
 		() => src(paths.pug.release)
 			.pipe(rename(`${r.number}.html`))
 			.pipe(pug(
-	    		Object.assign({'locals': r}, pug_config)
+	    		Object.assign({locals: r}, pug_config)
 	    	))
 			.pipe(dest(paths.out.html))
 		)
@@ -107,6 +114,21 @@ function resources() {
 		.pipe(dest(paths.out.base));
 }
 
+// Upload
+function upload() {
+	return src(paths.out.base+'**/*')
+		.pipe(s3upload({
+			Bucket: 'badoptics.co',
+			ACL: 'private',
+			maps:{
+				Metadata: () => s3meta,
+				CacheControl: () => s3cache,
+			},
+		},{
+			maxRetries: 5,
+		}));
+}
+
 // Cleanup
 function clean() {
 	return del(paths.out.base+'*');
@@ -118,4 +140,7 @@ exports.resources = resources
 exports.js = parallel(index_js, release_js);
 exports.css = css;
 exports.html = parallel(index, ...release_pages());
-exports.default = series(clean, parallel(exports.html, css, exports.js, resources));
+exports.build = series(clean, parallel(exports.html, css, exports.js, resources));
+exports.upload = series(upload, ()=>Promise.resolve(console.log("NOTE: Remember to perform invalidations in cloudfront")));
+exports.full = series(exports.build, exports.upload);
+exports.default = exports.build;
